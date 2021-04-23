f7a6e1a2dff34397a4ed603961b02d0d2756b132
Rimold Rynold <79126358+RimoldRynold@users.noreply.github.com>
2021-04-23 10:54:36 +0530

    Add files via upload

---
+++ evenlist.py
@@ -0,0 +1,6 @@
+l1=[12,24,45,13,16,1,18]
+even=[]
+for i in l1:
+	if(i%2==0):
+		even.append(i)
+print(even)
---
+++ new.py
@@ -0,0 +1,6 @@
+limit=int(input("enter the num of elements:"))
+l1=[]
+for i in range(0,limit):
+	item=int(input("enter the element:"))
+	l1.append
+	print(l1)
